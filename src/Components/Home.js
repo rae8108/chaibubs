0a06d92c2b7440932d2a298e5d1189e5322fcf6b
rae8108 <rayanbana01@gmail.com>
2024-12-28 15:56:16 +0000

    added content into home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import seasonalbanner from "../images/seasonalbanner.jpg"
+import Homegrid from "./Homegrid";
 import "./Home.css"
+
+import GridLayout from "./Gridlayout";
 
 export default function Home() {
 return (
@@ -8,6 +11,16 @@ return (
     <div>
         <img src={seasonalbanner} alt="seasonalbanner" className="seasonalbanner"/>
     </div>
+    <div>
+        <h4 className="welcometitle">Welcome to chaibubs </h4>
+        <p className="welcomemessage">We are a locally owned and renowned brand who adapts unique flavours to meet local needs, whilst keeping the traditional core value of Taiwanese bubble tea culture. We proudly serve fresh bubble tea at all our retail locations, where we aim to keep the core value of high-quality drinks, outstanding customer service, cleanliness and hygiene. Every season we adapt our special recipes to meet your needs. </p>
+    </div>
+  <div>
+    <Homegrid />
+  </div>
+  <div>
+    <GridLayout/>
+  </div>
 </div>
 )
 }
