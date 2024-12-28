8a173b29eb0fc23c701d3e97c1935b19951930db
rae8108 <rayanbana01@gmail.com>
2024-12-28 15:57:48 +0000

    created loyalty app button links + icons

---
+++ src/Components/Appicon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Appicon.css"
+
+export default function Appicon() {
+    return (
+        <div>
+            <div className="loyaltyapp">Download our loyalty app</div>
+            <div className="buttons">
+            <button>
+                <a href="https://apps.apple.com/gb/app/bubble-citea/id1659363993" target="_blank" rel="noopener noreferrer">App Store</a></button>
+            <button>
+                <a href="https://play.google.com/store/apps/details?id=com.como.prod940920221212&pli=1" target="_blank" rel="noopener noreferrer">Google Play</a></button>
+            </div>
+        </div>
+    )
+}
