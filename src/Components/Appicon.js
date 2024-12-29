753e2f33cb68644d513a9bb312010eeac541fd7b
rae8108 <rayanbana01@gmail.com>
2024-12-29 22:01:37 +0000

    updated appicon.js

--- src/Components/Appicon.js
+++ src/Components/Appicon.js
@@ -5,11 +5,11 @@ export default function Appicon() {
     return (
         <div>
             <div className="loyaltyapp">Download our loyalty app</div>
-            <div className="buttons">
+            <div>
+            <button className="buttons">
+                <a href="https://apps.apple.com/gb/app/bubble-citea/id1659363993" target="_blank" rel="noopener noreferrer" className="abuttons">App Store</a></button>
             <button>
-                <a href="https://apps.apple.com/gb/app/bubble-citea/id1659363993" target="_blank" rel="noopener noreferrer">App Store</a></button>
-            <button>
-                <a href="https://play.google.com/store/apps/details?id=com.como.prod940920221212&pli=1" target="_blank" rel="noopener noreferrer">Google Play</a></button>
+                <a href="https://play.google.com/store/apps/details?id=com.como.prod940920221212&pli=1" target="_blank" rel="noopener noreferrer" className="abuttons">Google Play</a></button>
             </div>
         </div>
     )
