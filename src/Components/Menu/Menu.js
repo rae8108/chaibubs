91463a9d8a4b943baf7ea5b969a73e2d8b247fd1
rae8108 <rayanbana01@gmail.com>
2025-01-03 21:04:52 +0000

    updated menu.js

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+import Footer from "../Footer/Footer";
 import "./Menu.css"
 
 export default function Menu() {
 return (
-    <p>home</p>
+<div>
+   <div className="menu-title">menu</div>
+    <div className="menu-grid-container">
+            <div className="menu-grid">
+               <p className="speciallink"><NavLink to="/special">Specials</NavLink></p>
+               <p className="speciallink"><NavLink to="/fruitteas">Fruit Teas</NavLink></p>
+               <p>Milk Teas</p>
+               <p>Toppings</p>
+            </div>
+        </div>
+        <div>
+            <Footer />
+        </div>
+        </div>
 )
 }
